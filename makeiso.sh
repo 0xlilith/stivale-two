b0da090d654f6440ed9012cba495abfca94967bc
0xlilith <0xlilith@protonmail.com>
2022-03-04 20:02:39 +0530

    add xorriso utility installation

--- makeiso.sh
+++ makeiso.sh
@@ -2,6 +2,9 @@
 
 # Download the latest Limine binary release.
 git clone https://github.com/limine-bootloader/limine.git --branch=v2.0-branch-binary --depth=1
+
+# Get xorriso utility
+apt-get install xorriso
  
 # Build limine-install.
 make -C limine
